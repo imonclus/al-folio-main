a115fbf6fb7dc115a827dab98a8c2d213309227b
imonclus <46054915+imonclus@users.noreply.github.com>
2026-05-26 08:23:50 +0000

    Deploying to gh-pages from @ imonclus/al-folio-main@22318d44665918a46da5d22a718e30603e341e98 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-sobre-mi",
-    title: "sobre mi",
+    id: "nav-inicio",
+    title: "inicio",
     section: "Navigation",
     handler: () => {
       window.location.href = "/al-folio-main/";
