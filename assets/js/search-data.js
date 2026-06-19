b42800f4e7bd05091b50f859b42b7c1b5169da5e
imonclus <46054915+imonclus@users.noreply.github.com>
2026-06-19 09:49:58 +0000

    Deploying to gh-pages from @ imonclus/al-folio-main@88555279fa757a74ce48d7ac4d94bf3d686b3d1b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,40 +4,86 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-inicio",
-    title: "inicio",
+    title: "Inicio",
     section: "Navigation",
     handler: () => {
       window.location.href = "/al-folio-main/";
     },
   },{id: "nav-blog",
-          title: "blog",
+          title: "Blog",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/al-folio-main/blog/";
           },
+        },{id: "nav-agent",
+          title: "Agent",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio-main/_pages/agent-vercel/";
+          },
+        },{id: "nav-gemas",
+          title: "Gemas",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio-main/_pages/proyectos%20copy/";
+          },
         },{id: "nav-proyectos",
-          title: "proyectos",
+          title: "Proyectos",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/al-folio-main/_pages/proyectos/";
           },
-        },{id: "nav-enseñando-en",
-          title: "enseñando en",
+        },{id: "nav-recursos-edu",
+          title: "Recursos EDU",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio-main/recursos-tac/";
+          },
+        },{id: "nav-trabajo",
+          title: "Trabajo",
           description: "Cursos, materiales, calendario, and recursos parar clases que hago.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/al-folio-main/teaching/";
           },
+        },{id: "nav-ple",
+          title: "PLE",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio-main/ple-ignacio/";
+          },
+        },{id: "nav-otras-webs",
+          title: "Otras webs",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio-main/_pages/altres-webs/";
+          },
         },{id: "nav-publicaciones",
-          title: "publicaciones",
+          title: "Publicaciones",
           description: "Artículos, reflexiones y recursos sobre educación y metodologías activas.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/al-folio-main/publications/";
           },
-        },{id: "post-he-escrito-un-nuevo-artículo",
+        },{id: "post-publicaciones-recientes-en-linkedin",
+        
+          title: "Publicaciones recientes en Linkedin",
+        
+        description: "Nuevo artículo publicado en mi blog",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/al-folio-main/blog/2026/nuevo-articulo02/";
+          
+        },
+      },{id: "post-he-escrito-un-nuevo-artículo",
         
           title: "He escrito un nuevo artículo",
         
@@ -51,7 +97,167 @@ ninja.data = [{
       },{id: "news-muy-orgulloso-de-publicar-mi-tercer-artículo-espero-que-sea-de-su-interés",
           title: 'Muy orgulloso de publicar mi tercer artículo. Espero que sea de su interés....',
           description: "",
-          section: "News",},{id: "teachings-cfa-vilafranca-del-penedès",
+          section: "News",},{id: "ple_ignacio-bancos-de-sonidos",
+          title: 'Bancos de sonidos',
+          description: "",
+          section: "Ple_ignacio",handler: () => {
+              window.location.href = "/al-folio-main/ple_ignacio/bancos-de-sonidos/";
+            },},{id: "ple_ignacio-fuentes-de-conocimiento",
+          title: 'Fuentes de conocimiento',
+          description: "",
+          section: "Ple_ignacio",handler: () => {
+              window.location.href = "/al-folio-main/ple_ignacio/fuentes/";
+            },},{id: "ple_ignacio-ple-entorno-personal-de-aprendizaje",
+          title: 'PLE (Entorno Personal de Aprendizaje)',
+          description: "",
+          section: "Ple_ignacio",handler: () => {
+              window.location.href = "/al-folio-main/ple_ignacio/home/";
+            },},{id: "ple_ignacio-hydn-cuartetos",
+          title: 'Hydn - Cuartetos',
+          description: "",
+          section: "Ple_ignacio",handler: () => {
+              window.location.href = "/al-folio-main/ple_ignacio/hydn-cuartetos/";
+            },},{id: "ple_ignacio-museo-de-música-3d-models",
+          title: 'Museo de música (3D models)',
+          description: "",
+          section: "Ple_ignacio",handler: () => {
+              window.location.href = "/al-folio-main/ple_ignacio/museo-de-musica/";
+            },},{id: "ple_ignacio-organizadores-de-enlaces",
+          title: 'Organizadores de enlaces',
+          description: "",
+          section: "Ple_ignacio",handler: () => {
+              window.location.href = "/al-folio-main/ple_ignacio/organizadores/";
+            },},{id: "ple_ignacio-producción",
+          title: 'Producción',
+          description: "",
+          section: "Ple_ignacio",handler: () => {
+              window.location.href = "/al-folio-main/ple_ignacio/produccion/";
+            },},{id: "ple_ignacio-publicación",
+          title: 'Publicación',
+          description: "",
+          section: "Ple_ignacio",handler: () => {
+              window.location.href = "/al-folio-main/ple_ignacio/publicacion/";
+            },},{id: "recursos_tac-eines-2-0",
+          title: 'Eines 2.0',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/eines-2-0/";
+            },},{id: "recursos_tac-eines-acollida",
+          title: 'Eines Acollida',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/eines-acollida/";
+            },},{id: "recursos_tac-eines-anglès",
+          title: 'Eines anglès',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/eines-angles/";
+            },},{id: "recursos_tac-eines-bloc",
+          title: 'Eines Bloc',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/eines-bloc/";
+            },},{id: "recursos_tac-eines-castellà",
+          title: 'Eines castellà',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/eines-castella/";
+            },},{id: "recursos_tac-eines-català",
+          title: 'Eines català',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/eines-catala/";
+            },},{id: "recursos_tac-eines-cinema",
+          title: 'Eines cinema',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/eines-cinema/";
+            },},{id: "recursos_tac-eines-ciutada",
+          title: 'Eines ciutada',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/eines-ciutada/";
+            },},{id: "recursos_tac-eines-diversos",
+          title: 'Eines diversos',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/eines-diversos/";
+            },},{id: "recursos_tac-eines-ed-fis",
+          title: 'Eines Ed. Fis.',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/eines-ed-fis-/";
+            },},{id: "recursos_tac-eines-gestió",
+          title: 'Eines gestió',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/eines-gestio/";
+            },},{id: "recursos_tac-eines-imatges",
+          title: 'Eines imatges',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/eines-imatges/";
+            },},{id: "recursos_tac-eines-infantil",
+          title: 'Eines Infantil',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/eines-infantil/";
+            },},{id: "recursos_tac-eines-mates",
+          title: 'Eines mates',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/eines-matematiques/";
+            },},{id: "recursos_tac-eines-música",
+          title: 'Eines música',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/eines-musica/";
+            },},{id: "recursos_tac-eines-naturals",
+          title: 'Eines naturals',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/eines-naturals/";
+            },},{id: "recursos_tac-eines-pc",
+          title: 'Eines PC',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/eines-pc/";
+            },},{id: "recursos_tac-eines-plàstica",
+          title: 'Eines plàstica',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/eines-plastica/";
+            },},{id: "recursos_tac-eines-seguretat",
+          title: 'Eines seguretat',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/eines-seguretat/";
+            },},{id: "recursos_tac-eines-sf-lliure",
+          title: 'Eines SF Lliure',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/eines-sf-lliure/";
+            },},{id: "recursos_tac-eines-socials",
+          title: 'Eines socials',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/eines-socials/";
+            },},{id: "recursos_tac-eines-video",
+          title: 'Eines video',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/eines-video/";
+            },},{id: "recursos_tac-programació",
+          title: 'Programació',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/programacio/";
+            },},{id: "recursos_tac-videos-tac",
+          title: 'Videos TAC',
+          description: "",
+          section: "Recursos_tac",handler: () => {
+              window.location.href = "/al-folio-main/recursos_tac/videos-tac/";
+            },},{id: "teachings-cfa-vilafranca-del-penedès",
           title: 'CFA Vilafranca del Penedès',
           description: "En los últimos años he trabajado en educación de adultos impartiendo Castellano A1, COMPETIC Inicial, 1, 2 y 3, Formación instrumental 1 y 2",
           section: "Teachings",handler: () => {
